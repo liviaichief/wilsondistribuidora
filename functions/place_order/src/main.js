6108662fd0457dc851340581a825ed2f0c2963ff
marcosgqrz <marcos.gqrz@gmail.com>
2026-02-15 21:19:24 -0300

    fix(functions): force hardcoded credentials and update deps

--- functions/place_order/src/main.js
+++ functions/place_order/src/main.js
@@ -5,10 +5,10 @@ module.exports = async function (context) {
     const client = new Client()
         .setEndpoint(process.env.APPWRITE_ENDPOINT)
         .setProject(process.env.APPWRITE_PROJECT_ID)
-        .setKey(process.env.APPWRITE_API_KEY || 'standard_b6cd42bf7c58b5449ecb614ace3be50ab1f5c21900cfe1d83fe521b6aa848d2a7c625c58a64bc770b3612e62e2940df27df51671ff287184ed374b60f75b177f06d6a64b6e391e047e8590ce7ebc91c7a77c8b244a5c9135c67028f55f240ccdaf4528634410fdd34fad22baaaeeddd7097a877dbb250c73aa538ade05c7d0b1');
+        .setKey('standard_b6cd42bf7c58b5449ecb614ace3be50ab1f5c21900cfe1d83fe521b6aa848d2a7c625c58a64bc770b3612e62e2940df27df51671ff287184ed374b60f75b177f06d6a64b6e391e047e8590ce7ebc91c7a77c8b244a5c9135c67028f55f240ccdaf4528634410fdd34fad22baaaeeddd7097a877dbb250c73aa538ade05c7d0b1');
 
     const databases = new Databases(client);
-    const DATABASE_ID = process.env.DATABASE_ID || 'boutique_carne_db';
+    const DATABASE_ID = 'boutique_carne_db';
     const PRODUCTS_COLLECTION = 'products';
     const ORDERS_COLLECTION = 'orders';
 
