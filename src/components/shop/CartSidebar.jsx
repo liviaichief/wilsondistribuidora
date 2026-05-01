d6236ea2cd0973292d9c51b6845376e4410187f4
Livia Ichief <liviaichief@gmail.com>
2026-04-30 22:47:40 -0300

    style: centralização vertical do preço no item do carrinho

--- src/components/shop/CartSidebar.jsx
+++ src/components/shop/CartSidebar.jsx
@@ -555,7 +555,7 @@ const CartSidebar = () => {
                                     <div key={item.id} className="cart-item">
                                         <img src={getImageUrl(item.image)} alt={formatTitleCase(item.title)} className="cart-item-img" />
                                         <div className="cart-item-info">
-                                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start', gap: '10px' }}>
+                                            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '10px' }}>
                                                 <h4>{formatTitleCase(item.title)}</h4>
                                                 <p className="cart-item-price">
                                                     {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(item.price)}
